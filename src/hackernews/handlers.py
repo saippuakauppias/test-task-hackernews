a778d64281bf8e890f754c1d3aca4a67e10650f1
Denis Veselov <progr-m@yandex.ru>
2020-07-14 21:54:32 +0300

    add limit & offset params to posts handler

--- src/hackernews/handlers.py
+++ src/hackernews/handlers.py
@@ -1,16 +1,27 @@
 from aiohttp import web
 
 from .models import Post
-from .utils import json_dumps
+from .utils import (json_dumps, validate_param_int)
 
 
 async def posts(request):
+    limit = request.query.get('limit', 5)
+    limit = validate_param_int(limit, 'wrong value in limit param')
+
+    offset = request.query.get('offset', 0)
+    offset = validate_param_int(offset, 'wrong value in offset param', False)
+
     async with request.app['db'].acquire() as conn:
-        cursor = await conn.execute(Post.select().with_only_columns([
+        stmt = Post.select().with_only_columns([
             Post.c.id,
             Post.c.title,
             Post.c.url
-        ]))
+        ]).limit(limit)
+
+        if offset:
+            stmt = stmt.offset(offset)
+
+        cursor = await conn.execute(stmt)
         records = await cursor.fetchall()
         posts = [dict(p) for p in records]
         return web.json_response(posts, dumps=json_dumps)
--- src/hackernews/utils.py
+++ src/hackernews/utils.py
@@ -2,6 +2,8 @@ import json
 import datetime
 import functools
 
+from aiohttp.web import HTTPBadRequest
+
 
 def datetime_json_converter(o):
     if isinstance(o, datetime.datetime):
@@ -9,3 +11,18 @@ def datetime_json_converter(o):
 
 
 json_dumps = functools.partial(json.dumps, default=datetime_json_converter)
+
+
+def validate_param_int(param, error_text, disallow_zero=True):
+    try:
+        param = int(param)
+        param = max(0, param)
+        param = min(param, 100)
+        if disallow_zero and not param:
+            raise ValueError()
+    except ValueError:
+        data = {'error': error_text}
+        data = json_dumps(data)
+        raise HTTPBadRequest(body=data)
+
+    return param
