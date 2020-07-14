db77da9c81462d942d64e0b506aaa553f2c35d2d
Denis Veselov <progr-m@yandex.ru>
2020-07-14 20:19:54 +0300

    fix columns in posts handler

--- src/hackernews/handlers.py
+++ src/hackernews/handlers.py
@@ -6,7 +6,11 @@ from .utils import json_dumps
 
 async def posts(request):
     async with request.app['db'].acquire() as conn:
-        cursor = await conn.execute(Post.select())
+        cursor = await conn.execute(Post.select().with_only_columns([
+            Post.c.id,
+            Post.c.title,
+            Post.c.url
+        ]))
         records = await cursor.fetchall()
         posts = [dict(p) for p in records]
         return web.json_response(posts, dumps=json_dumps)
