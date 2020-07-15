9b352c28b774fba5cc405c119dc6a69c16e76740
Denis Veselov <progr-m@yandex.ru>
2020-07-15 14:38:38 +0300

    run tests in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-exit_not_zero() {
+exit_non_zero() {
     if [ "$1" -ne 0 ]
     then
         >&2 echo "$2"
@@ -14,7 +14,7 @@ SLEEP_ITER=0
 SLEEP_MAX=60
 while ! nc -z "${DB_HOST}" "${DB_PORT}"; do
     if [ ${SLEEP_ITER} -ge ${SLEEP_MAX} ]; then
-        exit_not_zero '1' 'Database wait failed'
+        exit_non_zero '1' 'Database wait failed'
     fi
 
     echo "Waiting for database to run (${SLEEP_ITER})..."
@@ -26,14 +26,19 @@ done
 
 # initialize database
 hackernews init
-exit_not_zero "$?" 'Database init failed'
+exit_non_zero "$?" 'Database init failed'
 
 
 # run cron daemon in background
 /usr/sbin/crond -f -l 10 &
-exit_not_zero "$?" 'Cron run failed'
+exit_non_zero "$?" 'Cron run failed'
+
+
+# run tests (TODO: need only in dev?)
+pytest -v -l tests
+exit_non_zero "$?" 'Tests failed'
 
 
 # run server in foreground
 hackernews serve --port 80 --host 0.0.0.0
-exit_not_zero "$?" 'Server run failed'
+exit_non_zero "$?" 'Server run failed'
